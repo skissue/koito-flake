e1dbae5eb7e0030ff2a3613bc4d43a695335af67
Ad <me@skissue.xyz>
2025-10-13 20:52:44 -0400

    feat: Package for Koito

---
+++ package.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  pkg-config,
+  vips,
+  callPackage,
+}:
+buildGoModule (finalAttrs: {
+  pname = "Koito";
+  version = "0.0.13";
+
+  src = fetchFromGitHub {
+    owner = "gabehf";
+    repo = "Koito";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-n9SA/rGfc27fg0jz+3BaSgfF6ua3MDKBOwRSco+/qqc=";
+  };
+
+  vendorHash = "sha256-jxdPluTlyzs3jpa0yXPujaGsgi9KyaLMlUmI4fo4f60=";
+
+  passthru.ui = callPackage ./ui.nix {koito = finalAttrs.finalPackage;};
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [vips];
+
+  subPackages = ["cmd/api"];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  postInstall = ''
+    mv $out/bin/api $out/bin/koito
+    ln -s ${finalAttrs.passthru.ui} $out/client
+  '';
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://github.com/gabehf/Koito";
+    description = "Koito is a modern, themeable scrobbler that you can use with any program that scrobbles to ListenBrainz";
+    longDescription = ''
+      Koito is a modern, themeable ListenBrainz-compatible scrobbler for self-hosters who want control over their data and insights into their listening habits. It supports relaying to other compatible scrobblers, so you can try it safely without replacing your current setup.
+    '';
+    license = lib.licenses.mit;
+    mainProgram = "koito";
+  };
+})
---
+++ ui.nix
@@ -0,0 +1,38 @@
+{
+  koito,
+  stdenv,
+  fetchYarnDeps,
+  yarnConfigHook,
+  yarnBuildHook,
+  nodejs,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "${koito.pname}-client";
+  inherit (koito) version;
+
+  src = "${koito.src}/client";
+
+  yarnOfflineCache = fetchYarnDeps {
+    yarnLock = "${finalAttrs.src}/yarn.lock";
+    hash = "sha256-Z1mWNi55agNDvnTTt7cfVi7pQCBpQCACxVmyPvYAVHs=";
+  };
+
+  nativeBuildInputs = [
+    yarnConfigHook
+    yarnBuildHook
+    # Needed for executing package.json scripts
+    nodejs
+  ];
+
+  installPhase = ''
+    mkdir $out
+    cp -r build/ $out
+    cp -r public/ $out
+  '';
+
+  meta =
+    (removeAttrs koito.meta ["mainProgram"])
+    // {
+      description = "${koito.meta.description} - Client";
+    };
+})
